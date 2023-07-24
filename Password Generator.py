c0f64c1e9be9cc61bbb34729c40a6be838311cdd
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-07-24 18:00:11 +0430

    get the information

--- Password Generator.py
+++ Password Generator.py
@@ -1,6 +1,6 @@
 import random
 
-#creating lists contain numbers, letters and symbols
+#Create lists contain numbers, letters and symbols
 letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 
            'P', 'Q', 'R''S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c','e', 'f', 'g', 'h',
            'j', 'k', 'l', 'm', 'n','p','q','r','s','t','u','v','w','x','y','z']
@@ -9,3 +9,9 @@ numbers = ['1','2','3','4','5','6','7','8','9','0']
 
 symbols = ['!', '@','#', '$','%','^','&','*','(',')', '_', '+','-', '/', '*']
 
+
+#Get required information
+number_of_letters = int(input("How many letters would you like in your password? \n"))
+number_of_numbers = int(input("How many numbers would you like in your password? \n"))
+number_of_symbols = int(input("How many symbols would you like in your password? \n"))
+length_of_password = number_of_letters + number_of_symbols + number_of_numbers
